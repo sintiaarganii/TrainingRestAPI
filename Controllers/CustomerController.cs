4931dfac4a26f770676c316b7273bca75121915d
sintia dwi argani <sintiadwiargani@gmail.com>
2024-12-26 11:35:44 +0700

    NEW COMMMID ADD CRUD

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Training2.Models.DB;
 using Training2.services;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -10,6 +11,9 @@ namespace Training2.Controllers
     public class CustomerController : ControllerBase
     {
         private readonly CustomerServices _services;
+        private object _context;
+        private Customer customer;
+
         public CustomerController(CustomerServices services)
         {
             _services = services;
@@ -18,34 +22,71 @@ namespace Training2.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            //return new string[] { "value1", "value2" };
             var customerList = _services.GetListCustomers();
             return Ok(customerList);
         }
 
-        // GET api/<ValuesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult GetById(int id)
         {
-            return "value";
+            var showId = _services.GetById(id);
+            if (showId != null)
+            {
+                return Ok(showId);
+            }
+            return BadRequest("Id Not Found");
         }
 
         // POST api/<ValuesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult Post(Customer customer) //lebih fleksible 
         {
+            var insertCustomer = _services.createCustomer(customer);
+            if (insertCustomer)
+            {
+                return Ok("insert Customer success");
+            }
+            return BadRequest("insert Customer failed");
+            //return StatusCode(StatusCodes.) cari apa aja yg ada di statusCodes
         }
 
+        
         // PUT api/<ValuesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(Customer customer)
         {
+            try {
+                var updateCustomer = _services.UpdateCustomer(customer);
+                if (updateCustomer)
+                {
+                    return Ok("success");
+                }
+
+                return BadRequest("failed");
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message.ToString());
+                throw;
+            }
         }
 
         // DELETE api/<ValuesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            try
+            {
+                var DeleteCust = _services.DeleteCustomer(id);
+                if (DeleteCust)
+                {
+                    return Ok("success");
+                }
+                return BadRequest("Id Not Found");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
--- services/CustomerServices.cs
+++ services/CustomerServices.cs
@@ -1,4 +1,6 @@
-﻿using Training2.Models;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Training2.Models;
 using Training2.Models.DB;
 
 namespace Training2.services
@@ -15,6 +17,75 @@ namespace Training2.services
         {
             var datas = _context.Customers.ToList();
             return datas;
+        }
+
+        public Customer GetById(int id)
+        {
+            var customerId = _context.Customers.FirstOrDefault(x => x.id == id);
+            if(customerId != null)
+            {
+                return customerId;
+            }
+            return null;
+        }
+
+        public bool createCustomer(Customer customer)
+        {
+            try
+            {
+                _context.Customers.Add(customer);
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            try
+            {
+                var customerOld = _context.Customers.Where(x => x.id == customer.id).FirstOrDefault();
+                if (customerOld != null)
+                {
+                    customerOld.nama = customer.nama;
+                    customerOld.alamat = customer.alamat;
+                    customerOld.city = customer.city;
+                    customerOld.PhoneNumber = customer.PhoneNumber;
+
+
+                    _context.SaveChanges();
+
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            try
+            {
+                var customerData = _context.Customers.FirstOrDefault(x => x.id == id);
+
+                if (customerData != null)
+                {
+                    _context.Customers.Remove(customerData);
+                    _context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
