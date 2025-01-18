31bfe545c3962f9410905475d6ee39b2dffe95dc
sintia dwi argani <sintiadwiargani@gmail.com>
2025-01-18 21:28:59 +0700

    commit uncommited

--- obj/Debug/net8.0/Training2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Training2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Training2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+210fb97ae1849532c64c3ff95217499698f47a3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Training2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Training2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
