875327bfa3249c169713fa688700a9b4d083d31a
sintia dwi argani <sintiadwiargani@gmail.com>
2025-01-18 20:20:36 +0700

    Add Customer DTO (Data Transfer Object)

--- services/CustomerServices.cs
+++ services/CustomerServices.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Training2.Models;
 using Training2.Models.DB;
+using Training2.Models.DTO;
 
 namespace Training2.services
 {
@@ -13,27 +14,57 @@ namespace Training2.services
             _context = contex;
         }
 
-        public List<Customer> GetListCustomers()
+        public List<CustomerDTO> GetListCustomers()
         {
-            var datas = _context.Customers.ToList();
+            var datas = _context.Customers.Select(x => new CustomerDTO
+            {
+                Id = x.id.ToString(),
+                nama = x.nama,
+                alamat = x.alamat,
+                city = x.city,
+                PhoneNumber = x.PhoneNumber,
+                CreateDate = x.CreateDate != null ? x.CreateDate.Value.ToString("dd/MM/yyyy H:mm:ss") : "",
+                UpdateDate = x.UpdateDate != null ? x.UpdateDate.Value.ToString("dd/MM/yyyy H:mm:ss") : "",
+            }).ToList();
             return datas;
         }
 
-        public Customer GetById(int id)
+        public CustomerDTO GetCustomerById(int id)
         {
-            var customerId = _context.Customers.FirstOrDefault(x => x.id == id);
-            if(customerId != null)
+            var customer = _context.Customers.FirstOrDefault(x => x.id == id);
+            if (customer == null)
             {
-                return customerId;
+                return null;
             }
-            return null;
+
+            return new CustomerDTO
+            {
+                Id = customer.id.ToString(),
+                nama = customer.nama,
+                alamat = customer.alamat,
+                city = customer.city,
+                PhoneNumber = customer.PhoneNumber,
+                CreateDate = customer.CreateDate != null ? customer.CreateDate.Value.ToString("dd/MM/yyy HH:mm:ss") : "",
+                UpdateDate = customer.UpdateDate != null ? customer.UpdateDate.Value.ToString("dd/MM/yyy HH:mm:ss") : ""
+            };
         }
 
-        public bool createCustomer(Customer customer)
+
+        public bool createCustomer(CustomerReqDTO customer)
         {
             try
             {
-                _context.Customers.Add(customer);
+                var insertDataCust = new Customer
+                {
+                    nama = customer.nama,
+                    alamat = customer.alamat,
+                    city = customer.city,
+                    PhoneNumber = customer.PhoneNumber,
+                    CreateDate = DateTime.Now,
+                    UpdateDate = DateTime.Now,
+                };
+
+                _context.Customers.Add(insertDataCust);
                 _context.SaveChanges();
                 return true;
             }
@@ -43,18 +74,20 @@ namespace Training2.services
             }
         }
 
-        public bool UpdateCustomer(Customer customer)
+        public bool UpdateCustomer(int Id, CustomerReqDTO customer)
         {
             try
             {
-                var customerOld = _context.Customers.Where(x => x.id == customer.id).FirstOrDefault();
+                var customerOld = _context.Customers.Where(x => x.id == Id).FirstOrDefault();
+
                 if (customerOld != null)
                 {
                     customerOld.nama = customer.nama;
                     customerOld.alamat = customer.alamat;
                     customerOld.city = customer.city;
                     customerOld.PhoneNumber = customer.PhoneNumber;
-
+                    customerOld.CreateDate = customerOld.CreateDate;
+                    customerOld.UpdateDate = DateTime.Now;
 
                     _context.SaveChanges();
 
